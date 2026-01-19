80e626275f53006e9e1cc16a6fbb9c5bf9bae1fb
Kim Seung-Been <mainlib990@gmail.com>
2026-01-19 15:50:59 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/org/codeit/sb06/team03/mopl/MoplApplication.java
@@ -0,0 +1,13 @@
+package org.codeit.sb06.team03.mopl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codeit/sb06/team03/mopl/MoplApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codeit.sb06.team03.mopl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
