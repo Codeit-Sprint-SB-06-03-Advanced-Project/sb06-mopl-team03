afbdd2fe6e9d3355f7fb3caba8437f83f1431175
김도현 <henly5@naver.com>
2026-02-05 21:31:02 +0900

    feat: 아웃바운드 포트, 어댑터 구현

--- src/main/java/org/codeit/sb06/team03/mopl/account/application/AccountAppService.java
+++ src/main/java/org/codeit/sb06/team03/mopl/account/application/AccountAppService.java
@@ -1,21 +1,19 @@
 package org.codeit.sb06.team03.mopl.account.application;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.codeit.sb06.team03.mopl.account.application.in.RegisterAccountCommand;
 import org.codeit.sb06.team03.mopl.account.application.in.RegisterAccountUseCase;
 import org.codeit.sb06.team03.mopl.account.application.in.UpdatePasswordCommand;
 import org.codeit.sb06.team03.mopl.account.application.in.UpdatePasswordUseCase;
-import org.codeit.sb06.team03.mopl.account.application.out.CreateUserPort;
-import org.codeit.sb06.team03.mopl.account.application.out.LoadAccountPort;
-import org.codeit.sb06.team03.mopl.account.application.out.SaveAccountPort;
+import org.codeit.sb06.team03.mopl.account.application.out.*;
 import org.codeit.sb06.team03.mopl.account.domain.Account;
 import org.codeit.sb06.team03.mopl.account.domain.AccountService;
+import org.codeit.sb06.team03.mopl.account.domain.PasswordResetService;
 import org.codeit.sb06.team03.mopl.account.domain.entity.PasswordReset;
 import org.codeit.sb06.team03.mopl.account.domain.exception.AccountRegistrationFailedException;
 import org.codeit.sb06.team03.mopl.account.domain.exception.EmailAddressAlreadyExistsException;
-import org.codeit.sb06.team03.mopl.account.domain.policy.PasswordEncryptionPolicy;
 import org.codeit.sb06.team03.mopl.account.domain.vo.EmailAddress;
-import org.codeit.sb06.team03.mopl.account.domain.vo.Password;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,12 +22,16 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
+@Slf4j
 public class AccountAppService implements RegisterAccountUseCase, UpdatePasswordUseCase {
 
     private final AccountService accountService;
+    private final PasswordResetService passwordResetService;
     private final LoadAccountPort loadAccountPort;
     private final CreateUserPort createUserPort;
     private final SaveAccountPort saveAccountPort;
+    private final LoadPasswordResetPort loadPasswordResetPort;
+    private final DeletePasswordResetPort deletePasswordResetPort;
 
     @Override
     @Transactional
@@ -60,23 +62,26 @@ public class AccountAppService implements RegisterAccountUseCase, UpdatePassword
         final UUID accountUUID = parseUUID(command.accountId());
         Account account = loadAccountPort.findByAccountId(accountUUID)
                 .orElseThrow(() -> new RuntimeException("")); // TODO 커스텀예외
-
         PasswordReset passwordReset = loadPasswordResetPort.findByAccountId(accountUUID)
-                .orElseThrow(() -> new RuntimeException(""));
+                .orElseThrow(() -> new RuntimeException("")); // TODO 커스텀예외
 
         // 임시 비밀번호 검증
-        Password encryptedInput = passwordEncryptionPolicy.apply(command.tempPassword());
-
-        if (!passwordReset.validateTempPassword(encryptedInput)) {
-            throw new RuntimeException(); // TODO 커스텀예외
-        }
+        passwordResetService.validateTempPassword(passwordReset, command.tempPassword());
 
         // 새 비밀번호로 변경
         accountService.updatePassword(account, command.newPassword());
 
+        // 저장, 임시 비밀번호 삭제
         saveAccountPort.save(account);
+        deletePasswordResetPort.deleteByAccountId(accountUUID);
+    }
 
-        // 임시 비밀번호 삭제
-        deletePasswordResetPort.delete(passwordReset); // 임시 비밀번호 삭제
+    private UUID parseUUID(String id) {
+        try {
+            return UUID.fromString(id);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            // 비즈니스 계층에서 이해할 수 있는 예외로 변환
+            throw new InvalidIdentifierException(""); // TODO 커스텀예외
+        }
     }
 }
---
+++ src/main/java/org/codeit/sb06/team03/mopl/account/infra/out/DeletePasswordResetAdapter.java
@@ -0,0 +1,20 @@
+package org.codeit.sb06.team03.mopl.account.infra.out;
+
+import lombok.RequiredArgsConstructor;
+import org.codeit.sb06.team03.mopl.account.application.out.DeletePasswordResetPort;
+import org.codeit.sb06.team03.mopl.account.domain.entity.PasswordReset;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class DeletePasswordResetAdapter implements DeletePasswordResetPort {
+
+    private final PasswordResetRepository repository;
+
+    @Override
+    public void deleteByAccountId(UUID accountId) {
+        repository.deleteById(accountId);
+    }
+}
