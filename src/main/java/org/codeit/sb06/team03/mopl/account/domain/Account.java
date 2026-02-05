bcd352f63c4eba7e76d26c01425b90ee74d619cc
chiho0270 <96578548+chiho0270@users.noreply.github.com>
2026-02-05 13:55:53 +0900

    feat(account): PasswordReset 엔티티 및 entity 패키지 생성(package-info.java 포함) / Role 열거 추가 | Email -> EmailAddress 추가 리팩토링 | Account 엔티티 보완

--- src/main/java/org/codeit/sb06/team03/mopl/account/domain/Account.java
+++ src/main/java/org/codeit/sb06/team03/mopl/account/domain/Account.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.codeit.sb06.team03.mopl.account.domain.entity.PasswordReset;
 import org.codeit.sb06.team03.mopl.account.domain.vo.EmailAddress;
 import org.codeit.sb06.team03.mopl.account.domain.vo.Password;
 import org.springframework.data.annotation.CreatedDate;
@@ -20,18 +21,22 @@ import static org.codeit.sb06.team03.mopl.account.domain.event.AccountEvent.Acco
 @Getter
 @Entity
 @EntityListeners(AuditingEntityListener.class)
+@Table(name = "accounts")
 public class Account extends AbstractAggregateRoot<Account> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
     private UUID id;
 
     @NotNull
     @CreatedDate
-    @Column(nullable = false, updatable = false)
+    @Column(name = "created_at", nullable = false)
     private Instant createdAt;
 
+    @NotNull
     @Version
+    @Column(name = "version", nullable = false)
     private short version;
 
     @Embedded
@@ -40,10 +45,23 @@ public class Account extends AbstractAggregateRoot<Account> {
     @Embedded
     private Password password;
 
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Column(name = "locked", nullable = false)
+    private boolean locked;
+
+    @OneToOne(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+    private PasswordReset passwordReset;
+
     public static Account create(EmailAddress emailAddress, Password password) {
         var account = new Account();
         account.emailAddress = emailAddress;
         account.password = password;
+        account.role = Role.USER;
+        account.locked = false;
         account.registerEvent(new AccountRegisteredEvent());
         return account;
     }
