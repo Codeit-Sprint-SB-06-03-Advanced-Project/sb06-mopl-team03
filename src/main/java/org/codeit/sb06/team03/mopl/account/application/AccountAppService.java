5076f8b6a12d606fd1ffcabfd5f638af224a0d36
LeeJinWoo <jionu102@naver.com>
2026-02-05 21:30:49 +0900

    feat(account): AccountAppService에 assignRole 구현

--- src/main/java/org/codeit/sb06/team03/mopl/account/application/AccountAppService.java
+++ src/main/java/org/codeit/sb06/team03/mopl/account/application/AccountAppService.java
@@ -1,6 +1,8 @@
 package org.codeit.sb06.team03.mopl.account.application;
 
 import lombok.RequiredArgsConstructor;
+import org.codeit.sb06.team03.mopl.account.application.in.AssignRoleCommand;
+import org.codeit.sb06.team03.mopl.account.application.in.AssignRoleUseCase;
 import org.codeit.sb06.team03.mopl.account.application.in.RegisterAccountCommand;
 import org.codeit.sb06.team03.mopl.account.application.in.RegisterAccountUseCase;
 import org.codeit.sb06.team03.mopl.account.application.out.CreateUserPort;
@@ -8,16 +10,18 @@ import org.codeit.sb06.team03.mopl.account.application.out.LoadAccountPort;
 import org.codeit.sb06.team03.mopl.account.application.out.SaveAccountPort;
 import org.codeit.sb06.team03.mopl.account.domain.Account;
 import org.codeit.sb06.team03.mopl.account.domain.AccountService;
-import org.codeit.sb06.team03.mopl.account.domain.exception.AccountRegistrationFailedException;
-import org.codeit.sb06.team03.mopl.account.domain.exception.EmailAddressAlreadyExistsException;
+import org.codeit.sb06.team03.mopl.account.domain.Role;
+import org.codeit.sb06.team03.mopl.account.domain.exception.*;
 import org.codeit.sb06.team03.mopl.account.domain.vo.EmailAddress;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
-public class AccountAppService implements RegisterAccountUseCase {
+public class AccountAppService implements RegisterAccountUseCase, AssignRoleUseCase {
 
     private final AccountService accountService;
     private final LoadAccountPort loadAccountPort;
@@ -43,5 +47,36 @@ public class AccountAppService implements RegisterAccountUseCase {
 
         saveAccountPort.save(newAccount);
         return newAccount;
+    }
+
+    @Override
+    @Transactional
+    public void assignRole(String userId, AssignRoleCommand command) {
+        // 제공받은 프로토 타입은 user-account를 분리하지 않았지만
+        // 이벤트 스토밍 과정에서 user와 account가 분리되었고
+        // 프론트엔드는 고정되어 있기에 현재 저희 프로젝트에서 userId는 AccountId의 의미로 사용되고 있습니다.
+        String accountId = userId;
+        UUID accountUuid = parseUUID(accountId);
+
+        if (!Role.contains(command.role())) {
+            throw new InvalidRoleException(command.role());
+        }
+
+        Role role = Role.valueOf(command.role());
+
+        Account foundAccount = loadAccountPort.findById(accountUuid)
+                .orElseThrow(() -> new AccountNotFoundException(accountUuid));
+
+        Account updatedAccount = accountService.updateRole(foundAccount, role);
+
+        saveAccountPort.save(updatedAccount);
+    }
+
+    private UUID parseUUID(String id) {
+        try {
+            return UUID.fromString(id);
+        } catch (IllegalArgumentException e) {
+            throw new InvalidAccountIdFormatException(id);
+        }
     }
 }
