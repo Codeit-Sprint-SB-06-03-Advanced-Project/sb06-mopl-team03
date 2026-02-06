329307f012e51066270264c8a4c383d86cc9d2fb
jionu102 <128346722+jionu102@users.noreply.github.com>
2026-02-06 10:16:29 +0900

    feat(account): 계정 권한 수정 (#56)
    * [FEAT] 회원가입 기능 구현 (#46)
    
    * chore: Java/Spring-Boot 버전 다운그레이
    
    * chore: lombok 의존성 추가
    
    * chore: resilience 의존성 추가
    
    * fix: lombok 의존성 수정
    
    * chore: springdoc 의존성 추가
    
    * chore: 개발 환경 설정
    
    - OSIV 설정 끔
    - 기본 프로파일 설정 dev로 지정
    - dev 프로파일 추가
    - dev 환경 H2로 데이터소스 설정
    
    * feat(account): package-info.java 추가
    
    * feat(user): package-info.java 추가
    
    * feat(common): 공통 모듈 설정 추가
    
    - 프로퍼티 스캔
    - JPA Auditing
    - 글로벌 에러 핸들러
    
    * feat(account): 계정 기능 프로퍼티 추가
    
    * feat(account): 계정 회원가입 도메인 레이어 구현
    
    * feat(account): 계정 회원가입 어플리케이션 레이어 구현
    
    * feat(account): 계정 회원가입 인프라 레이어 구현
    
    * feat(bff): BffUserService 추가
    
    * feat: account AR, 추가 디자인에 따른 엔티티 추가 및 어트리뷰트 추가 (+ EmailAddress 네이밍 리팩토링) (#47)
    
    * [FEAT] 이메일 네이밍 email -> emailAddress
    
    * [refator] 이메일 네이밍 변경 email -> emailAddress
    
    * refactor(account): exception의 package-info 채움
    
    * feat(account): PasswordReset 엔티티 및 entity 패키지 생성(package-info.java 포함) / Role 열거 추가 | Email -> EmailAddress 추가 리팩토링 | Account 엔티티 보완
    
    * refactor(account): 불필요한 @NotNull 제거
    
    ---------
    
    Co-authored-by: Kim Seung-Been <mainlib990@gmail.com>
    
    * feat(account): Role enum에 contains 메서드 추가
    
    * feat(account): Account 도메인이 updateRole 메서드 추가
    
    * feat(account): 권한 수정 관련 예외 클래스 추가
    
    * feat(account): AccountService에 updateRole메서드 추가
    
    * feat(account): LoadAccountPort에 findById 메서드 추가
    
    * feat(account): AssignRoleUseCase 및 Command 추가
    
    * feat(account): AccountAppService에 assignRole 구현
    
    * feat(account): 권한 수정 API 엔드포인트 구현
    
    * feat(account): 잘못된 h2 의존성 삭제
    
    ---------
    
    Co-authored-by: mainlib990 <43126937+mainlib990@users.noreply.github.com>
    Co-authored-by: chiho0270 <96578548+chiho0270@users.noreply.github.com>
    Co-authored-by: Kim Seung-Been <mainlib990@gmail.com>

--- src/main/java/org/codeit/sb06/team03/mopl/account/infra/out/LoadAccountAdapter.java
+++ src/main/java/org/codeit/sb06/team03/mopl/account/infra/out/LoadAccountAdapter.java
@@ -2,8 +2,12 @@ package org.codeit.sb06.team03.mopl.account.infra.out;
 
 import lombok.RequiredArgsConstructor;
 import org.codeit.sb06.team03.mopl.account.application.out.LoadAccountPort;
+import org.codeit.sb06.team03.mopl.account.domain.Account;
 import org.codeit.sb06.team03.mopl.account.domain.vo.EmailAddress;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Component
@@ -14,5 +18,10 @@ public class LoadAccountAdapter implements LoadAccountPort {
     @Override
     public boolean existsByEmailAddress(EmailAddress emailAddress) {
         return repository.existsByEmailAddress(emailAddress);
+    }
+
+    @Override
+    public Optional<Account> findById(UUID accountId) {
+        return repository.findById(accountId);
     }
 }
