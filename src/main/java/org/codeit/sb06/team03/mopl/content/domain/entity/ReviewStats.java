a906f04337ddda4def246d2bbd397b4bbd8b96bb
Kim Seung-Been <mainlib990@gmail.com>
2026-02-09 14:22:48 +0900

    refactor(content): import 최적화

--- src/main/java/org/codeit/sb06/team03/mopl/content/domain/entity/ReviewStats.java
+++ src/main/java/org/codeit/sb06/team03/mopl/content/domain/entity/ReviewStats.java
@@ -1,7 +1,6 @@
 package org.codeit.sb06.team03.mopl.content.domain.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
