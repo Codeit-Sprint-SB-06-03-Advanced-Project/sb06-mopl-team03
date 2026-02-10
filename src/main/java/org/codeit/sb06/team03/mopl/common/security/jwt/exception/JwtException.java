b58c0f21e5dc60a9be9d90a1af62e92e83a442f4
LeeJinWoo <jionu102@naver.com>
2026-02-10 18:47:50 +0900

    chore(auth): 패키지 정리에 따른 파일 이동

--- src/main/java/org/codeit/sb06/team03/mopl/common/security/jwt/exception/JwtException.java
+++ src/main/java/org/codeit/sb06/team03/mopl/common/security/jwt/exception/JwtException.java
@@ -1,6 +1,7 @@
-package org.codeit.sb06.team03.mopl.common.security.jwt;
+package org.codeit.sb06.team03.mopl.common.security.jwt.exception;
 
 public abstract class JwtException extends RuntimeException {
+
     protected JwtException(String message) {
         super(message);
     }
