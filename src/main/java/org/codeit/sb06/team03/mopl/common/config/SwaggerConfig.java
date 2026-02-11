620458c86b01bf01c275388baf13757c0a0f7351
LeeJinWoo <jionu102@naver.com>
2026-02-11 19:08:49 +0900

    feat(auth): JWT 인증을 위한 Swagger 설정 추가

---
+++ src/main/java/org/codeit/sb06/team03/mopl/common/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package org.codeit.sb06.team03.mopl.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(
+                        new SecurityRequirement().addList("Bearer Authentication")
+                )
+                .components(
+                        new Components().addSecuritySchemes(
+                                "Bearer Authentication", createSecurityScheme()
+                        )
+                )
+                .info(
+                        new Info()
+                                .title("Mopl REST API")
+                                .description("codeit-sb06 team03 mopl")
+                                .version("1.0")
+                );
+    }
+
+    private SecurityScheme createSecurityScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
