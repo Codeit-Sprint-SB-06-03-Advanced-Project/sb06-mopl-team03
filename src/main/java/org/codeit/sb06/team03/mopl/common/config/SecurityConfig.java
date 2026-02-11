295fe9c32cdd3ca45fc6fa102b36bbaf74a8e2f7
LeeJinWoo <jionu102@naver.com>
2026-02-11 21:47:00 +0900

    feat(auth): CSRF 보안 기능 추가 및 에러 핸들링 - sessionCreationPolicy stateless 설정 - 401, 403 에러 핸들링 - CSRF 보안 추가 및 spa환경 핸들러 추가

--- src/main/java/org/codeit/sb06/team03/mopl/common/config/SecurityConfig.java
+++ src/main/java/org/codeit/sb06/team03/mopl/common/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.codeit.sb06.team03.mopl.common.config;
 
 import org.codeit.sb06.team03.mopl.account.domain.vo.Role;
 import org.codeit.sb06.team03.mopl.common.security.LoginFailureHandler;
+import org.codeit.sb06.team03.mopl.common.security.SpaCsrfTokenRequestHandler;
 import org.codeit.sb06.team03.mopl.common.security.jwt.JwtAuthenticationFilter;
 import org.codeit.sb06.team03.mopl.common.security.jwt.JwtLoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +13,12 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -22,21 +27,31 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(
             HttpSecurity http,
+            SpaCsrfTokenRequestHandler spaCsrfTokenRequestHandler,
             JwtLoginSuccessHandler loginSuccessHandler,
             LoginFailureHandler loginFailureHandler,
-            JwtAuthenticationFilter jwtAuthenticationFilter
+            JwtAuthenticationFilter jwtAuthenticationFilter,
+            AuthenticationEntryPoint authenticationEntryPoint,
+            AccessDeniedHandler accessDeniedHandler
     ) throws Exception {
         http
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(spaCsrfTokenRequestHandler)
+                        .ignoringRequestMatchers("/h2-console/**")
+                        .ignoringRequestMatchers("/v3/api-docs/**", "/swagger-ui/**")
+                )
                 .headers(headers -> headers
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .authorizeHttpRequests(auth -> auth
                         // 정적 리소스, h2, swagger
                         .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers("/v3/api-docs", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .requestMatchers("/", "/index.html", "/favicon.svg", "/assets/**").permitAll()
                         // REST API
                         .requestMatchers(HttpMethod.POST, "/api/auth/sign-in").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/auth/csrf-token").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(login -> login
@@ -44,7 +59,13 @@ public class SecurityConfig {
                         .successHandler(loginSuccessHandler)
                         .failureHandler(loginFailureHandler)
                 )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                        .accessDeniedHandler(accessDeniedHandler)
+                )
         ;
 
         return http.build();
