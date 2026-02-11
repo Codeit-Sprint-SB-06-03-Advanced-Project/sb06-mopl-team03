2cddcb5e49b2d84781654490ce764dbceda9aaa2
LeeJinWoo <jionu102@naver.com>
2026-02-11 19:08:05 +0900

    chore(auth): 필요없는 import 제거

--- src/main/java/org/codeit/sb06/team03/mopl/common/security/LoginFailureHandler.java
+++ src/main/java/org/codeit/sb06/team03/mopl/common/security/LoginFailureHandler.java
@@ -1,7 +1,6 @@
 package org.codeit.sb06.team03.mopl.common.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
