8f455f6f8045099a176424a153b9394dd5708eec
LeeJinWoo <jionu102@naver.com>
2026-02-23 14:23:05 +0900

    feat(live-chat): WatchingSessionCommandService 구현

---
+++ src/main/java/org/codeit/sb06/team03/mopl/watchingSession/application/WatchingSessionCommandService.java
@@ -0,0 +1,59 @@
+package org.codeit.sb06.team03.mopl.watchingSession.application;
+
+import lombok.RequiredArgsConstructor;
+import org.codeit.sb06.team03.mopl.watchingSession.application.in.CreateWatchingSessionCommand;
+import org.codeit.sb06.team03.mopl.watchingSession.application.in.CreateWatchingSessionUseCase;
+import org.codeit.sb06.team03.mopl.watchingSession.application.in.DeleteWatchingSessionUseCase;
+import org.codeit.sb06.team03.mopl.watchingSession.application.in.GetWatchingSessionUseCase;
+import org.codeit.sb06.team03.mopl.watchingSession.application.out.DeleteWatchingSessionPort;
+import org.codeit.sb06.team03.mopl.watchingSession.application.out.LoadWatchingSessionPort;
+import org.codeit.sb06.team03.mopl.watchingSession.application.out.SaveWatchingSessionPort;
+import org.codeit.sb06.team03.mopl.watchingSession.domain.WatchingSession;
+import org.codeit.sb06.team03.mopl.watchingSession.domain.exception.WatchingSessionDuplicateException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional(readOnly=true)
+@RequiredArgsConstructor
+public class WatchingSessionCommandService implements
+        CreateWatchingSessionUseCase, DeleteWatchingSessionUseCase, GetWatchingSessionUseCase {
+
+    private final SaveWatchingSessionPort saveWatchingSessionPort;
+    private final LoadWatchingSessionPort loadWatchingSessionPort;
+    private final DeleteWatchingSessionPort deleteWatchingSessionPort;
+
+    @Override
+    @Transactional
+    public void create(CreateWatchingSessionCommand command) {
+        UUID liveChatId = command.liveChatId();
+        UUID watcherId = command.watcherId();
+
+        if (loadWatchingSessionPort.existsByLiveChatIdAndWatcherId(liveChatId, watcherId)) {
+            throw WatchingSessionDuplicateException.fromLiveChatIdAndAccountId(liveChatId, watcherId);
+        }
+
+        WatchingSession watchingSession = WatchingSession.create(watcherId, liveChatId);
+        saveWatchingSessionPort.save(watchingSession);
+    }
+
+    @Override
+    @Transactional
+    public void deleteByWatcherId(UUID watcherId) {
+        deleteWatchingSessionPort.deleteByWatcherId(watcherId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteByLiveChatIdAndWatcherId(UUID liveChatId, UUID watcherId) {
+        deleteWatchingSessionPort.deleteByLiveChatIdAndWatcherId(liveChatId, watcherId);
+    }
+
+    @Override
+    public List<WatchingSession> get(UUID watcherId) {
+        return loadWatchingSessionPort.findByWatcherId(watcherId);
+    }
+}
