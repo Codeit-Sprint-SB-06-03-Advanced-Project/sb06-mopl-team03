c8202a66fdf0259542479bd3f2f2eb75b9af3708
LeeJinWoo <jionu102@naver.com>
2026-02-23 15:52:03 +0900

    feat(live-chat): Content Exception 정의

---
+++ src/main/java/org/codeit/sb06/team03/mopl/liveChat/domain/exception/ContentNotFoundException.java
@@ -0,0 +1,18 @@
+package org.codeit.sb06.team03.mopl.liveChat.domain.exception;
+
+import org.codeit.sb06.team03.mopl.content.domain.exception.ContentException;
+
+import java.util.UUID;
+
+public class ContentNotFoundException extends ContentException {
+
+    private static final String fromIdFormat = "Content를 찾을 수 없습니다. id: '%s'";
+
+    public ContentNotFoundException(String message) {
+        super(message);
+    }
+
+    public static ContentNotFoundException fromId(UUID id) {
+        return new ContentNotFoundException(fromIdFormat.formatted(id));
+    }
+}
